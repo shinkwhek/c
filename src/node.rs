75b5b77b442c307f8c7124d486bc23dbc8c4463d
shinkwhek <shinkwhek@gmail.com>
2018-08-24 01:48:39 +0900

    [add] node.rs

---
+++ src/node.rs
@@ -0,0 +1,23 @@
+#[derive(Debug, PartialEq)]
+pub enum NodeBase {
+    BinaryOp(Box<Node>, Box<Node>, BinOp),
+    Number(usize),
+    Nope,
+}
+
+#[derive(Debug, PartialEq)]
+pub enum BinOp {
+    Add,
+    Sub,
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Node {
+    pub base: NodeBase,
+}
+
+impl Node {
+    pub fn new(base: NodeBase) -> Node {
+        Node { base: base }
+    }
+}
